a3012fee4afe42c81afaed38682971415ea3a3f4
Zephyr <ugd.zephyr@gmail.com>
2022-09-23 02:24:19 +0200

    adding pointer 10

---
+++ pointer/w3_pointer10.c
@@ -0,0 +1,8 @@
+/* Programmer: Per Stoor
+ * Date: 2022-09-15
+ * Last changed: 2022-09-15
+ * Type of program: Calculate the length of the string using a pointer
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
