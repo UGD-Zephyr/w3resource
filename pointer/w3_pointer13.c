543a03ab54d3f89634a41ef482713434f765653d
Zephyr <ugd.zephyr@gmail.com>
2022-09-26 15:52:04 +0200

    adding pointer exercise 13

---
+++ pointer/w3_pointer13.c
@@ -0,0 +1,83 @@
+/* Programmer: Per Stoor
+ * Date: 2022-09-25
+ * Last changed: 2022-09-26
+ * Type of program: count the number of vowels and consonants in a string using a pointer.
+ */
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <ctype.h>
+
+#define STRING_LENGTH 50
+
+void vowel_and_consonant_counter(char *string_pointer);
+
+int main(int argc, char *argv[]){ 
+
+    char user_inputted_string[STRING_LENGTH];
+
+        printf("Enter string:");
+        fgets(user_inputted_string, STRING_LENGTH, stdin);
+    
+        vowel_and_consonant_counter(user_inputted_string);
+    
+return 0;
+} 
+
+void vowel_and_consonant_counter(char *string_pointer){
+
+    int function_vowel_counter;
+    int function_consonant_counter;
+    int function_loop_counter1;
+
+        function_vowel_counter = 0;
+        function_consonant_counter = 0;
+
+             
+             
+             
+            for(function_loop_counter1 = 0; string_pointer[function_loop_counter1] != '\0'; function_loop_counter1++){
+
+               /* 
+                * First the string gets converted to all lowercase to make
+                * the vowel and consonant check twice as fast (we don't need
+                * to show the user that we only check lowercase letters)
+                */
+                string_pointer[function_loop_counter1] = tolower(string_pointer[function_loop_counter1]);
+
+               /* 
+                * The first if statement checks for big and small letters in the string,
+                * if none, consonant and vowels will not be counted.
+                */
+                if( (string_pointer[function_loop_counter1] >= 65 && string_pointer[function_loop_counter1] <= 90) ||
+                    (string_pointer[function_loop_counter1] >= 97 && string_pointer[function_loop_counter1] <= 122)){
+                    
+                    
+                   /* 
+                    * The second if statement looks for vowels to count,
+                    * if none count as consonant in the below else statement. 
+                    */        
+                    if (
+                        (string_pointer[function_loop_counter1] == 97) ||
+                        (string_pointer[function_loop_counter1] == 101)||
+                        (string_pointer[function_loop_counter1] == 105)||
+                        (string_pointer[function_loop_counter1] == 111)||
+                        (string_pointer[function_loop_counter1] == 117)){
+
+                            function_vowel_counter++;
+                    }
+                   /* 
+                    * Since the nested if statements filter out all non-alphabet characters
+                    * we don't need to worry about white spaces either.
+                    */
+                    else{
+                            function_consonant_counter++;
+                    }
+                }
+
+            }
+
+        printf("Vowels:     %d\n", function_vowel_counter);
+        printf("Consonants: %d\n", function_consonant_counter);
+
+}
