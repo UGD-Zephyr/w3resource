6d823f9a21ca4102d25d98c7c0d4101e36165e20
Zephyr <ugd.zephyr@gmail.com>
2022-09-27 13:44:55 +0200

    adding exercise 14

--- pointer/w3_pointer14.c
+++ pointer/w3_pointer14.c
@@ -12,30 +12,78 @@
 
 #define STRING_LENGTH 10
 
+void array_print_function(int *dummy_array, int array_size);
+int *array_input_function(int array_size);
+
 int main(int argc, char *argv[]){
 
 	int *number_array;
-	int user_inputted_string[STRING_LENGTH];
 	int number_array_element;
-	int user_inputted_value;
+    char user_inputted_string[STRING_LENGTH];
 
-		number_array = (int *) malloc(number_array_element * sizeof(number_array_element));
-			if(number_array == NULL){
-				printf("error allocating memory!\n");
-				exit(EXIT_FAILURE);
-			}
+		printf("Enter amount of elements:");
+		fgets(user_inputted_string, STRING_LENGTH, stdin);
+		sscanf(user_inputted_string, "%d", &number_array_element);
 
-			printf("Enter amount of elements:");
-			fgets(user_inputted_string, STRING_LENGTH, stdin);
-			sscanf(user_inputted_string, "%d", &number_array_element);
+        /*
+         * Function to input selected amount of elements
+         * into the array.
+         * */
+        //array_input_function(number_array, number_array_element);
 
-				for(loop_counter1 = 0; loop_counter1 < number_array_element; loop_counter1++){
-					printf("Enter element %d:", loop_counter1+1);
-					fgets(user_inputted_string, STRING_LENGTH, stdin);
-					sscanf(user_inputted_string, "%d", &user_inputted_value);
-				}
+        /*
+         * Function to 
+         * */
+		number_array = array_input_function(number_array_element);
 
+        /*
+         * Array before sorting.
+         * */
+        printf("\nUnsorted array:   ");
+        array_print_function(number_array, number_array_element);
+        printf("\n");
+
+        /*
+         * Array after sorting.
+         * */
+        printf("\nSorted array:     ");
+        array_print_function(number_array, number_array_element);
+        printf("\n");
+       
 		free(number_array);
 
 return 0;
+}
+
+void array_print_function(int *dummy_array_print, int array_print_size){
+
+    int function_loop_counter1;
+
+        for(function_loop_counter1 = 0; function_loop_counter1 < array_print_size; function_loop_counter1++){
+            printf("%d ", (*dummy_array_print + function_loop_counter1));
+        }
+}
+
+int *array_input_function(int array_input_size){
+
+    int *function_array;
+    int function_loop_counter1;
+	int function_inputted_value;
+    char function_inputted_string[STRING_LENGTH];
+
+        function_array = (int *) malloc(array_input_size * sizeof(array_input_size));
+			if(function_array == NULL){
+				printf("error allocating memory!\n");
+				exit(EXIT_FAILURE);
+			}
+            
+            for(function_loop_counter1 = 0; function_loop_counter1 < array_input_size; function_loop_counter1++){
+                printf("Enter element %d:", function_loop_counter1+1);
+                fgets(function_inputted_string, STRING_LENGTH, stdin);
+                sscanf(function_inputted_string, "%d", &function_inputted_value);
+                *function_array = function_inputted_value;
+                *function_array++;
+		    }        
+
+return function_array;
 }
